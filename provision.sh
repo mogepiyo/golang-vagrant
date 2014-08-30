783c4decc9ebab32f39b1dc8b8d102c24c14432a
mogepiyo <coffee.potato+mogepiyo@gmail.com>
2014-08-31 00:10:33 +0900

    Escape commands correctly

--- provision.sh
+++ provision.sh
@@ -55,8 +55,8 @@ su vagrant -c "ln -s /usr/local/go/misc/vim/indent/go.vim /home/vagrant/.vim/ind
 su vagrant -c "ln -s /usr/local/go/misc/vim/compiler/go.vim /home/vagrant/.vim/compiler"
 su vagrant -c "ln -s /usr/local/go/misc/vim/plugin/godoc.vim /home/vagrant/.vim/plugin"
 # Install my vimrc
-su vagrant -c git clone https://gist.github.com/4953ccde3ac74fd00bfa.git ~/vimrc
-su vagrant -c cat ~/vimrc >> .vimrc
-su vagrant -c rm -rf ~/vimrc
-su vagrant -c mkdir -p ~/.vim/bundle
-su vagrant -c git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+su vagrant -c "git clone https://gist.github.com/4953ccde3ac74fd00bfa.git ~/vimrc"
+su vagrant -c "cat ~/vimrc >> .vimrc"
+su vagrant -c "rm -rf ~/vimrc"
+su vagrant -c "mkdir -p ~/.vim/bundle"
+su vagrant -c "git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim"
